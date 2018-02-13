32021e3ba5752609daaeef8c93e3d714997c3ce5
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-13 09:30:39 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -80,10 +80,11 @@ def renderPage1():
 
 @app.route('/page2')
 def renderPage2():
-    if 'public_repos' in session['user_data']:
-        pur = session['user_data']['public_repos']
+    if 'user_data' in session:
+        if 'public_repos' in session['user_data']:
+            pur = session['user_data']['public_repos']
     else:
-        pur = 0;
+        pur = 0
     return render_template('page2.html', pr=pur)
 
 #the tokengetter is automatically called to check who is logged in
