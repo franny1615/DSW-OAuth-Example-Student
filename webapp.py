7c5eb2572b97a86833c3196e03f1f9337033e075
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-13 09:11:26 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -80,11 +80,10 @@ def renderPage1():
 
 @app.route('/page2')
 def renderPage2():
+    pur = 0
     if 'public_repos' in session['user_data']:
-        p_r = session['user_data']['public_repos']
-    else:
-        p_r = 0
-    return render_template('page2.html', pr=p_r)
+        pur = session['user_data']['public_repos']
+    return render_template('page2.html', pr=pur)
 
 #the tokengetter is automatically called to check who is logged in
 @github.tokengetter
